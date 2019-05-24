672b7599d9473fb08e4e419ad64af7af2f00a04c
tanacchi <q111026d@mail.kyutech.jp>
2019-05-25 01:12:14 +0900

    Add function to get tops of Pile

--- include/pile.hpp
+++ include/pile.hpp
@@ -10,6 +10,7 @@ class Pile
     Pile() noexcept;
     Pile(CellMatrix&& cells) noexcept;
     const CellMatrix& get_data() const;
+    std::vector<std::size_t> get_tops() const noexcept;
 
   private:
     CellMatrix body_;
